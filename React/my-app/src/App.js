935a50a3a8ded3adda8084aa4af06a6b90f51671
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-10-17 11:42:51 +0200

    React Course (Components & Props)

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,4 +1,25 @@
 import './App.css';
 
+function App (){
+  return (  <div className="App">
+      <Job salary={9000} position='Senior SDE' company='Amazon' />
+      <Job salary={12000} position='Junior SDE' company='Google' />
+      <Job salary={10000} position='Project Manager' company='Netflix' />
+    </div>
+  );
+}
+
+const Job = (props) => {
+return(
+  <div>
+  <h1>Name of company:{props.company}</h1>
+  <br/>
+  <h1>Position :{props.position}</h1>
+  <br/>
+  <h1>Salary of Employee :{props.salary}</h1>
+  <br/>
+  </div>
+);
+}
 
 export default App;
