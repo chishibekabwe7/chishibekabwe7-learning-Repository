4c3b67be167de4d73693131dd2f5bc3870b3ab29
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-10-17 13:06:22 +0200

    React Course (Loops and Arrays) Task-1

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-import {Job} from './tasks/Job';
 
 function App (){
+  const planets = [ 
+    {name: "Mars", isGasPlanet: false},
+    {name: "Earth", isGasPlanet: false},
+    {name: "Jupiter", isGasPlanet: true},
+    {name: "Venus", isGasPlanet: false},
+    {name: "Neptune", isGasPlanet: true},
+    {name: "Uranus", isGasPlanet: true},
+  ];
+
   return (  <div className="App">
-      <Job salary={9000} position='Senior SDE' company='Amazon' />
-      <Job salary={12000} position='Junior SDE' company='Google' />
-      <Job salary={10000} position='Project Manager' company='Netflix' />
+    
+      {planets.map((value, key) =>{
+        return <h1 key={key}> {value.name} </h1>
+      })} 
     </div>
   );
 }
