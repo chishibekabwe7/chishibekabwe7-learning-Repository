49851a482ead5ecb21aebd08b1f8718a60891ea1
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-10-21 22:27:38 +0200

    React Course (States) Task 3

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import { useState } from 'react';
 
 function App (){
-  const [textColor, setTextColor] = useState("black");
+  const [count , setCount] = useState(0);
+
+  const IncreaseBy1 = () => {
+    setCount(count + 1);
+  };
+  const DecreaseBy1 = () => {
+    setCount(count - 1);
+  };
+  const Reset = () => {
+     setCount(0)
+  };
+
 
   return (  
-
   <div className="App">
-    <button onClick={() => {setTextColor(textColor === "black" ? "red" :"black")}}>Show/Hide</button>
-    <h1 style={{color:textColor}}> Hi my name is Chishibe</h1>
+   <button onClick={DecreaseBy1}>Decrease</button><br/>
+   <button onClick={IncreaseBy1} style={{marginTop : "10px"}}>Increase</button><br/>
+   <button onClick={Reset} style={{marginTop : "10px"}}>Set to Zero</button><br/><br/>
+   {count}
   </div>
   );
 }
