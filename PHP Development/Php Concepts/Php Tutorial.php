904918fb0ab485968598d0c7f26a56e02c0620e2
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-10-27 11:46:34 +0200

    Php Arrays

--- PHP Development/Php Concepts/Php Tutorial.php
+++ PHP Development/Php Concepts/Php Tutorial.php
@@ -7,6 +7,7 @@
 </head>
 <body>
   <?php 
+  /*
   //Variables
   $my_first_variable = "learning from Simplilearn";
   $my_first_number_variable = 5;
@@ -16,6 +17,21 @@
   echo "<h1>" . $my_first_variable ."<h1>";
   echo "<h2>" . $my_first_number_variable ."<h2>";
   echo "<h3>" . $my_first_float_variable ."<h3>";
+  */
+  
+  //Arrays
+  //Method 1
+  $laptop_brand = array("HP","Lenovo","Asus","Dell");
+  
+  //Array Output
+  echo var_dump($laptop_brand);
+  echo var_dump($laptop_brand[2]);
+  
+
+ //Method 2
+  define("Tech_Giants", ["Apple","Microsoft","Google","Amazon"]);
+  echo Tech_Giants[3];
+
   ?>
 </body>
 </html>
