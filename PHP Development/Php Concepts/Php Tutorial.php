c737eea3fd879b332f840e946b05c432f497ed7d
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-10-27 11:21:27 +0200

    Php Variables

---
+++ PHP Development/Php Concepts/Php Tutorial.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+  <?php 
+  //Variables
+  $my_first_variable = "learning from Simplilearn";
+  $my_first_number_variable = 5;
+  $my_first_float_variable =7.5;
+
+  //Variable Output
+  echo "<h1>" . $my_first_variable ."<h1>";
+  echo "<h2>" . $my_first_number_variable ."<h2>";
+  echo "<h3>" . $my_first_float_variable ."<h3>";
+  ?>
+</body>
+</html>
