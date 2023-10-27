9008bc04da14b9ba3c8efc16cc3342386cee271f
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-10-27 06:46:26 +0200

    React CRUD TodoList (Day 1)

--- React Development/my-app/src/App.js
+++ React Development/my-app/src/App.js
@@ -2,29 +2,57 @@ import './App.css';
 import { useState } from 'react';
 
 function App (){
- const [todoList,setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  
+  {/*OnChange Function */}
+  const handleChange = (event) =>{
+    setNewTask(event.target.value);
+  }
 
- const [newTask,setNewTask] = useState("");
- 
- const handleChange = (event) =>{
-  setNewTask(event.target.value);
- }
+  {/*Add Task Function */}
+  const addTask = () => {
+    setTodoList([...todoList ,newTask]);
+  }
 
- const addTask = () => {
+  {/*Delete Task Function */}
+  const deleteTask = (taskName) => {
+   const newTodoList = todoList.filter((task) =>{
+    if (task === taskName){
+      return false;
+    }
+    else{
+      return true;
+    }
+   }); 
+   setTodoList(newTodoList);
+};
 
- };
   return (  
-  <div className="App">
-    {/*div 1*/}
-    <div className="addTask">
-      <input onChange={handleChange} />
-        <button>Add Task</button><br/>
-        {newTask}
-    </div>
+<div className="App">
 
-  {/*div 2 */}
-  <div className="list"></div>
-  </div>
+  {/*Div 1*/}
+   <div className='addTask'>
+    <input onChange={handleChange}/> 
+      <button onClick={addTask}> Add Task </button>
+   </div> 
+    {newTask}
+
+
+  {/*Div 2*/}
+   <div className='list'>
+   {todoList.map((task =>{
+    return <div>
+      <h1>{task}</h1>
+      <button onClick={deleteTask}>X</button>
+      </div>
+              
+   }))} 
+    </div> 
+
+
+</div>
+
   );
 }
 
