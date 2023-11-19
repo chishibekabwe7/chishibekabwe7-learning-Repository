b34a66223ad1fb8d9458f0d50ff929b487d0f2a2
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-11-19 14:52:33 +0200

    C Programming Booleans

---
+++ C Programming (Fundamentals)/Booleans.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h> //Don't forget to add this preprocessor or else the booleans wont run
+
+int main (){
+bool isProgrammingFun = true;
+bool isProgrammingBoring = false;
+
+printf("%d\n",isProgrammingFun);    //outputs 1 if its true
+printf("%d\n",isProgrammingBoring); //outputs 0 if its false 
+    return 0;
+}
