144a47a869a4078a59526cece45a11573849fad6
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-11-19 12:18:00 +0200

    C++ Programming Variables

---
+++ C++ Programming (Fundamentals)/Variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main () {
+    //Snake case variable
+    string My_var = "This is a Snake case variale";
+    cout<<My_var<<endl;
+
+    //This is a Camel case Variable
+    string myVar2 = "This is a Camel case variable";
+    cout<<myVar2<<endl;
+    
+    
+    
+   //This is a Pascal case Variable
+    string MyVar3 = "This is a Pascal case variable";
+    cout<<MyVar3<<endl;
+
+    /*
+    Kebab case is not susport by C++ programming
+   //This is a kebab case variable 
+   string My-Var4 = "This is a kebab case variable";
+   cout<<My-Var4<<endl;
+    */
+    return 0;
+}
