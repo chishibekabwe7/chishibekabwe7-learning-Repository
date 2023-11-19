20f6dca806e89aa4dc3685e4837a02f3d70fe588
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-11-19 12:29:24 +0200

    React CRUD TodoList (Day 2)

--- React Development/my-app/src/App.js
+++ React Development/my-app/src/App.js
@@ -1,61 +1,50 @@
 import './App.css';
 import { useState } from 'react';
+import {Task} from './tasks/Task.js'
 
-function App (){
+function App() {
   const [todoList, setTodoList] = useState([]);
   const [newTask, setNewTask] = useState("");
-  
-  {/*OnChange Function */}
-  const handleChange = (event) =>{
+
+  // OnChange Function
+  const handleChange = (event) => {
     setNewTask(event.target.value);
-  }
+  };
 
-  {/*Add Task Function */}
+  // Add Task Function
   const addTask = () => {
-    setTodoList([...todoList ,newTask]);
-  }
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
+    setNewTask(""); // Clear the input field after adding a task
+  };
 
-  {/*Delete Task Function */}
-  const deleteTask = (taskName) => {
-   const newTodoList = todoList.filter((task) =>{
-    if (task === taskName){
-      return false;
-    }
-    else{
-      return true;
-    }
-   }); 
-   setTodoList(newTodoList);
-};
+  // Delete Task Function
+  const deleteTask = (id) => {
+    const newTodoList = todoList.filter((task) => {
+      return task.id !== id; // Keep tasks with different IDs
+    });
+    setTodoList(newTodoList);
+  };
 
-  return (  
-<div className="App">
-
-  {/*Div 1*/}
-   <div className='addTask'>
-    <input onChange={handleChange}/> 
-      <button onClick={addTask}> Add Task </button>
-   </div> 
-    {newTask}
-
-
-  {/*Div 2*/}
-   <div className='list'>
-   {todoList.map((task =>{
-    return <div>
-      <h1>{task}</h1>
-      <button onClick={deleteTask}>X</button>
+  return (
+    <div className="App">
+      {/* Input bar & Add Task button */}
+      <div className='addTask'>
+        <input value={newTask} onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
       </div>
-              
-   }))} 
-    </div> 
 
-
-</div>
-
+      {/* Displaying the list item with an array */}
+      <div className='list'>
+        {todoList.map((task) => (
+         <task /> 
+        ))}
+      </div>
+    </div>
   );
 }
-
-
 
 export default App;
