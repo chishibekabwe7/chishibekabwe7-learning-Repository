efdf2da0579d93b4a32d8a76d616c90d959cb333
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-11-29 00:46:47 +0200

    Python Programming While loop

---
+++ Python Programming (Fundamentals)/While loop.py
@@ -0,0 +1,19 @@
+#while loop
+#The break Statement
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+  
+#The continue Statement
+"""
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+  
+"""
