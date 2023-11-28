0e17be5e7ed8392a6a44c98b63d7e012807a4f92
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-11-29 00:18:37 +0200

    Python Programming Sets Array

---
+++ Python Programming (Fundamentals)/Sets.py
@@ -0,0 +1,16 @@
+#Declaring a set
+my_set = ['jordan', 'ellesse','nike','adidas','Vans off the wall','Off white' 'Cactus Jack', 'Palm angles','Dior','Essentials fear of god']
+print(my_set)
+
+#Accessing the set elements with indexing
+print(my_set[1])
+
+#Accessing the set elements with negative indexing
+print(my_set[-1])
+
+#looping through the set elements
+for x in my_set:
+    print(x)
+
+#Checking the set length
+print(len(my_set))
