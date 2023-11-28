8c44713ba4af4eb3bbe224281410102cf2effd22
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2023-11-29 00:52:25 +0200

    Python Programming For loop

---
+++ Python Programming (Fundamentals)/For loop.py
@@ -0,0 +1,14 @@
+#Python3 for loop
+faang_companies = ['facebook','apple','amazon','netflix','google']
+for x in faang_companies:
+    print(x)
+
+    
+#looping through a string
+my_string = 'faang'
+for y in my_string:
+    print(y)
+
+#The range function
+for z in range(6):
+  print(z)
