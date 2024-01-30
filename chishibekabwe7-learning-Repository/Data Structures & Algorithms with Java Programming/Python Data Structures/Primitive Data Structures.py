454d37af6e27c2fc5e843810bc27888c7af9536b
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2024-01-31 01:16:34 +0200

    Primitive Data Structures Tutorial with Python Day 1

---
+++ chishibekabwe7-learning-Repository/Data Structures & Algorithms with Java Programming/Python Data Structures/Primitive Data Structures.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+#Python Has 4 built in data types or primitive Data Structures which include Lists,Tuples,Sets,and Dictionaries.
+
+# 1.Python Lists
+"""
+1. Lists are used to store multiple items in a single variable,lists are one of the four built in data types
+Lists,Tuples,Sets and Dictionary.
+2. Lists can have any multiple datatypes.
+3.  
+"""
+
+#Lists
+my_list = ["The Copperbelt University","University of Zambia","Mukuba University","University of Lusaka",1,True,34]
+print(type(my_list))
+#Outputing it out to the console
+print(my_list)
+#Iterating through the list
+for x in my_list:
+    print(x)
+#Ordering the List
+"""
+my_list.sort()
+print(my_list)
+"""
+
+#Tuples
+my_tuple = ["duplicates are allowed","can be ordered","Chishibe Kabwe","Can be ordered",34,22,7]
+
+
+
---
+++ chishibekabwe7-learning-Repository/Data Structures & Algorithms with Java Programming/Python Data Structures/Sets.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#Declaring a set
+my_set = {'jordan', 'ellesse','nike','adidas','Vans off the wall','Off white' 'Cactus Jack', 'Palm angles','Dior','Essentials fear of god'}
+print(my_set)
+
+#Accessing the set elements with indexing
+#print(my_set[1])
+
+#Accessing the set elements with negative indexing
+#print(my_set[-1])
+
+#looping through the set elements
+for x in my_set:
+    print(x)
+
+#Checking the set length
+print(len(my_set))
+
+#Checking the data type
+print(type(my_set))
