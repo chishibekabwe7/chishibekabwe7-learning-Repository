27779b9972f9a7c2ed72b6ab2a2ba1ffea5f8243
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2024-02-05 00:03:30 +0200

    Added Firebase Analytics to Project(Major's Electronic Hub)

---
+++ chishibekabwe7-learning-Repository/Firebase/Majors's Electronic Hub/public/js/firebase.js
@@ -0,0 +1,21 @@
+// Importing SDK Functions
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyCNU0ac2RlXDvvEAI5oFY6KeJQD2tZXlIs",
+authDomain: "major-s-electronic-hub.firebaseapp.com",
+projectId: "major-s-electronic-hub",
+storageBucket: "major-s-electronic-hub.appspot.com",
+messagingSenderId: "1094084959124",
+appId: "1:1094084959124:web:75783c36caff87f28a4d81",
+measurementId: "G-06TG16DRS3"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);  
+       
+/*================================ Developed by ChishibeKabwe7@Github =============================================================================*/
