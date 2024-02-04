13ab1f4939375c8fa58c77d7a69acbed07577e01
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2024-02-05 00:02:54 +0200

    Added Firebase Analytics to Project(Kay Graphics)

---
+++ chishibekabwe7-learning-Repository/Firebase/Kay Graphics/public/js/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyAS9njQ_LZTADgeLEYz9hIcW4v6ZhZizps",
+authDomain: "kay-graphics.firebaseapp.com",
+projectId: "kay-graphics",
+storageBucket: "kay-graphics.appspot.com",
+messagingSenderId: "504454158464",
+appId: "1:504454158464:web:491c8acf9dbdb90583ee65",
+measurementId: "G-1LW1HJTB2C"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);  
+/*================================ Developed by ChishibeKabwe7@Github =============================================================================*/
+
