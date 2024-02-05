c6e19928b451150cf29d725a801a068368e9eb5a
chishibekabwe7 <121695530+chishibekabwe7@users.noreply.github.com>
2024-02-05 02:31:21 +0200

    Queue Data Structure with Python Day 1

---
+++ chishibekabwe7-learning-Repository/Data Structures & Algorithms with Java Programming/Python Data Structures/Queue Data Structure.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# 1. Queue Data Structure with Lists
+#Declaring a Queue Data Structure with a list
+my_queue = []
+
+#Enqueueing elements
+my_queue.append(10.00)
+my_queue.append(20.00)
+my_queue.append(30.00)
+my_queue.append(40.00)
+my_queue.append(50.00)
+my_queue.append(60.00)
+my_queue.append(70.00)
+my_queue.append(80.00)
+my_queue.append(90.00)
+my_queue.append(100.00)
+
+#Dequeue elements
+my_queue.pop()
+my_queue.pop()
+my_queue.pop()
+my_queue.pop()
+
+#Dequeued element and storing it in a variable
+Dequeued_element = my_queue.pop()
+print(f"{Dequeued_element} was recently dequeued from Chishibe's queue")
+
+#Checking the length of the queue
+queue_length = len(my_queue)
+print(f"The length of Chishibe's queue is {queue_length}")
+
+#Checking if queue is empty
+if my_queue:
+    print("Chishibe's queue is not empty")
+else:
+    print("Chishibe's queue is empty")
+
+#Accessing the top element with a variable
+top_element = my_queue[-1]
+print(f"The top element in Chishibe's queue is {top_element}")
+
+
---
+++ chishibekabwe7-learning-Repository/My Algorithms/Queue Data Structure Operation Algorithms in Python.md
@@ -0,0 +1,8 @@
+## Algorithm to perform operations on Queue Data Structure with python by Chishibe Kabwe.
+
+1. Declare a Queue.
+2. Add elements to your queue.
+3. Remove elements from queue
+4. Check if the queue is empty.
+5. Check the length of the queue.
+6. Access the top element with a variable.
