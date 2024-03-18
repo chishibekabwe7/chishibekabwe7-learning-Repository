786eaff7cf9499576f582c854d5b3723cdf36136
chishibekabwe7 <chishibekabwe7@gmail.com>
2024-03-17 21:06:54 -0400

    Reconfigured files in repository

--- chishibekabwe7-learning-Repository/Firebase/Projects Crud App/functions/server.js
+++ chishibekabwe7-learning-Repository/Firebase/Projects Crud App/functions/server.js
@@ -1,3 +1,37 @@
+const express = require('express')
+
+const app = express();
+app.get('/',(req,res) => {
+    res.send("Crud Application")
+})
+
+app.listen(3000, ()=> {console.log(`Server is running on localhost:${3000}`)})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Import function triggers from their respective submodules:
  *
@@ -10,10 +44,3 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
