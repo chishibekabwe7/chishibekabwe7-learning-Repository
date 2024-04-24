59038b80b550898886ddfd5ca7c9559a167eff7a
chishibekabwe7 <chishibekabwe7@gmail.com>
2024-04-24 20:49:16 +0200

    Initialized git in WSL

---
+++ chishibekabwe7-learning-Repository/chishibekabwe7-learning-Repository/React Development/my-second-app/src/tasks/UseEffect example tutorial.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { useState} from "react";
+import { Text } from "./UseEffect example Tutorial";
+
+function App() {
+  const [showText,setShowText] = useState(false);
+  
+
+    return(
+        <div className="App">
+          <button
+            onClick={() =>{
+              setShowText(!showText);
+            }}>Show Text
+          </button>
+          {showText && <Text />}
+        </div>
+    )
+  }
+  
+
+
+export default App;
+
